d24875f7aa987505bb522e3965bc87cf89e61662
Rafal <rafal.opial@coderslab.pl>
2021-07-17 11:25:31 +0200

    Refactor GloappsRegistrationPage to use PageFactory.initElements(...)

--- src/test/java/pl/coderslab/pageobjectpatternonlteaw02/registration/GloappsRegistrationPage.java
+++ src/test/java/pl/coderslab/pageobjectpatternonlteaw02/registration/GloappsRegistrationPage.java
@@ -1,19 +1,20 @@
 package pl.coderslab.pageobjectpatternonlteaw02.registration;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 // this is our class implementing a page object pattern
 public class GloappsRegistrationPage {
-    private final WebDriver driver;
+    @FindBy(id = "email_create")
+    WebElement emailInput;
 
     public GloappsRegistrationPage(WebDriver driver) {
-        this.driver = driver;
+        PageFactory.initElements(driver, this);
     }
 
     public void fillForm(String email) {
-        WebElement emailInput = driver.findElement(By.cssSelector("#email_create"));
         emailInput.clear();
         emailInput.sendKeys(email);
     }
